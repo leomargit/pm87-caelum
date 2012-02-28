3be7d024854783cd937abea264574c034b50ce94
leomar <lero.github@fastcoding.net>
2012-02-27 22:49:58 -0300

    commit 2

--- src/classe.java
+++ src/classe.java
@@ -2,6 +2,6 @@
 public class classe {
 	public static void main(String[] args) {
 		System.out.println("Exemplo de teste de uso do git");
-		System.out.println("Commit 1");
+		System.out.println("Commit 2");
 	}
 }
